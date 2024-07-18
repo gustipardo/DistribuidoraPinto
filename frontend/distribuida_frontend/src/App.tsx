352252d6f860870dac5ee50766e95d6ebae28f41
gustipardo <gusm2003@gmail.com>
2024-07-18 13:23:35 -0300

    Fullstack test working

--- frontend/distribuida_frontend/src/App.tsx
+++ frontend/distribuida_frontend/src/App.tsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [invoices, setInvoices] = useState([]);
+
+  useEffect(() => {
+    // Función asincrónica para cargar datos
+    async function fetchInvoices() {
+      try {
+          const response = await fetch('http://localhost:1234/invoices'); // Realiza la petición GET a http://localhost:1234/invoices
+        if (!response.ok) {
+          throw new Error('Error getting invoices');
+        }
+        const data = await response.json(); // Convierte la respuesta a JSON
+        setInvoices(data); // Guarda los datos de las facturas en el estado
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    fetchInvoices(); // Llama a la función para cargar datos al montar el componente
+  }, []); // El segundo argumento [] asegura que useEffect se ejecute solo una vez al montar el componente
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="App">
+        <h1>Listado de Facturas</h1>
+        <div >invoices: {invoices}</div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
